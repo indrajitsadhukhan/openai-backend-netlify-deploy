98c9c4f0e13542e3898afacc8b3af46cdeb9c340
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2023-03-06 12:35:24 +0530

    Included File upload APIs

--- src/api.js
+++ src/api.js
@@ -14,7 +14,8 @@ const api_key=process.env.OPENAI_API_KEY
 const configuration = new Configuration({
     apiKey: api_key,
   });
-  
+
+
 const openai = new OpenAIApi(configuration);
 
 // Create completion
@@ -27,7 +28,7 @@ router.post('/completion', async (req, res) => {
         model: "text-davinci-003",
         prompt: `${prompt}`,
         temperature: 0,
-        max_tokens: 2000, 
+        max_tokens: 4000, 
         top_p: 1, 
         frequency_penalty: 0.5,
         presence_penalty: 0, 
