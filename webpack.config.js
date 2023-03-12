2ab87600d4699bcee6f8e79aec6263d85e003871
indrajitsadhukhan <indrajitsadhukhan1000@gmail.com>
2023-03-12 22:08:49 +0530

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,10 @@
-export const target = 'node';
+{
+    // rest of the webpack config
+    resolve: {
+      // ... rest of the resolve config
+      fallback: {
+        "path": require.resolve("path-browserify")
+      }
+    },
+  }
+  
