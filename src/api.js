a358e6c6199cc78b79ce0889d9907495a064e6f4
Indrajit sadhukhan <indrajitsadhukhan1000@gmail.com>
2023-04-26 12:17:17 +0530

    Update with completion apis

--- src/api.js
+++ src/api.js
@@ -21,23 +21,23 @@ router.post('/completion', async (req, res) => {
       const prompt = JSON.parse(req.apiGateway.event.body).prompt;
       // console.log(prompt)
 
-      // const response = await openai.createCompletion({
-      //   model: "text-davinci-003",
-      //   prompt: `${prompt}`,
-      //   temperature: 0,
-      //   max_tokens: 2000, 
-      //   top_p: 1, 
-      //   frequency_penalty: 0.5,
-      //   presence_penalty: 0, 
-      // });
-
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: `${prompt}`}],
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 2000, 
+        top_p: 1, 
+        frequency_penalty: 0.5,
+        presence_penalty: 0, 
       });
+
+//       const response = await openai.createChatCompletion({
+//         model: "gpt-3.5-turbo",
+//         messages: [{role: "user", content: `${prompt}`}],
+//       });
       
       res.status(200).send({
-        bot: response.data.choices[0].message.content
+        bot: response.data.choices[0].text
       });
   
     } catch (error) {
