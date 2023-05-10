6a8836d45637937efb7b78af299772fad890463b
Indrajit sadhukhan <indrajitsadhukhan1000@gmail.com>
2023-05-10 18:33:18 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -20,25 +20,39 @@ router.post('/completion', async (req, res) => {
       // Prompt = Content + Instruction
       const prompt = JSON.parse(req.apiGateway.event.body).prompt;
       // console.log(prompt)
+        
+         const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 300, 
+      top_p: 1, 
+      frequency_penalty: 0.5,
+      presence_penalty: 0, 
+    });
 
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 0,
-        max_tokens: 400, 
-        top_p: 1, 
-        frequency_penalty: 0.5,
-        presence_penalty: 0, 
-      });
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+//       const response = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: `${prompt}`,
+//         temperature: 0,
+//         max_tokens: 400, 
+//         top_p: 1, 
+//         frequency_penalty: 0.5,
+//         presence_penalty: 0, 
+//       });
 
 //       const response = await openai.createChatCompletion({
 //         model: "gpt-3.5-turbo",
 //         messages: [{role: "user", content: `${prompt}`}],
 //       });
       
-      res.status(200).send({
-        bot: response.data.choices[0].text
-      });
+//       res.status(200).send({
+//         bot: response.data.choices[0].text
+//       });
   
     } catch (error) {
       // console.error(error)
