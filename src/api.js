274bb1d83ba4bb4d63124e4e038d79d7a28a49b6
Indrajit sadhukhan <indrajitsadhukhan1000@gmail.com>
2023-05-10 18:28:19 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -25,7 +25,7 @@ router.post('/completion', async (req, res) => {
         model: "text-davinci-003",
         prompt: `${prompt}`,
         temperature: 0,
-        max_tokens: 2000, 
+        max_tokens: 400, 
         top_p: 1, 
         frequency_penalty: 0.5,
         presence_penalty: 0, 
