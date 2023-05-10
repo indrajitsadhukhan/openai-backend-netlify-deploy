17d03c4c621b2437213829862b0ae11336f21515
Indrajit sadhukhan <indrajitsadhukhan1000@gmail.com>
2023-05-10 19:22:18 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -23,7 +23,7 @@ router.post('/completion', async (req, res) => {
         
          const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: prompt
+      prompt: prompt,
       temperature: 0,
       max_tokens: 200, 
       top_p: 1, 
